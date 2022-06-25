64edb780b0ec2ca8b8eae215db1e8c7bbf13033c
Rohit Omar <rohitomar@Rohits-MacBook-Air.local>
2022-06-25 17:52:18 +0530

    Enable Author Route

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const express = require('express')
 const app = express()
 const expressLayout = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayout)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true
@@ -22,5 +25,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', routes)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
