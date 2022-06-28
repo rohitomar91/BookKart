34c91a3e868c9f49df1eba3ac8c39e6e044fdd41
Rohit Omar <rohitomar@Rohits-MacBook-Air.local>
2022-06-29 00:57:51 +0530

    Fix for non-required package nodemon

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { reset } = require('nodemon')
 const router = express.Router()
 const Author = require('../models/authors')
 const Book = require('../models/books')
